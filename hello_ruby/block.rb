9f9a781dbdc9dd3c7dfeff56b0754ecda1e62583
tzuchuan.huang <tzuchuan.huang@rakuten.com>
2021-11-04 14:25:54 +0800

    add class & block

--- hello_ruby/block.rb
+++ hello_ruby/block.rb
@@ -37,3 +37,50 @@ p pick([*1..10]) { |x| x % 2 == 0} # [2,4,6,8,10]
 p pick([*1..10]) { |x| x - 2 == 0} # [2]
 p pick([*1..10]) { |x| x < 8} # [1,2,3,4,5,6,7]
 p pick([*1..10]) { |x| x.odd?} # [1,3,5,7,9]
+
+# pick([*1..10]) { |x|
+#     return x % 2 == 0
+# }
+# LocalJumpError, block 執行區段比較像是程式執行區塊， 不能 return, 因為不是 method.
+
+(5).times {
+    puts "print something"
+}
+
+def my_times(n) 
+    i = 0
+    while n > i
+        i += 1
+        yield(i)
+        # yield i
+    end
+end
+
+my_times(5) { |num|
+    puts "hello print something #{num}"
+}
+
+# 大括號和 do...end 其實有細微的不同
+p [*1..10].map { |i| i * 2}
+p [*1..10].map do |i| i * 2 end
+# 因為優先層級不同 實際上是這個樣子
+p ([*1..10].map { |i| i * 2})
+p ([*1..10].map) do |i| i * 2 end
+
+p [*1..10].map do
+    |i| i * 2 
+end
+
+
+greeting = Proc.new { |var|
+    puts "哈囉，世界"
+    puts "#{var}"
+} # 使用 Proc 類別可以把 Block 物件化
+# greeting.call
+
+greeting.call("我變成物件了！")
+greeting.("我變成物件了！")
+greeting["我變成物件了！"]
+greeting === "我變成物件了！"
+greeting.yield "我變成物件了！"
+greeting.yield("我變成物件了！")
--- hello_ruby/class.rb
+++ hello_ruby/class.rb
@@ -5,3 +5,64 @@ kitty = Cat.new
 kitty.name = "Nancy"
 puts kitty.name
 
+class Dog
+    def initialize(name, gender)
+        @name = name
+        @gender = gender
+    end
+
+    def eat(food)
+        puts "#{food} 好好吃，汪！"
+    end
+
+    def say
+        puts "我的名字是 #{@name}, 我的性別是 #{@gender}"
+    end
+
+    def name
+        @name
+    end
+
+    # 另外撰寫方法來改變實體變數
+    def name=(new_name)
+        @name = new_name
+    end
+
+    def gender 
+        @gender
+    end
+end
+
+boen = Dog.new("伯恩山的第一隻狗", "male")
+boen.eat("骨頭")
+boen.say
+shib = Dog.new("柴柴", "female")
+shib.eat("狗食")
+shib.say
+print shib.name, shib.gender
+
+shib.name = "我是改名的柴柴！"
+puts shib.name
+
+class Sheep
+    attr_accessor :name
+    attr_accessor :gender
+
+    def initialize(name, gender)
+        @name = name
+        @gender = gender
+    end
+
+    def say
+        puts "我的名字是 #{@name}, 我是一隻羊！ 我的性別是 #{@gender}"
+    end
+end
+
+fat = Sheep.new("fat fat", "male")
+fat.say
+fat.name = "slim fat"
+fat.gender = "female"
+fat.say
+
+
+
