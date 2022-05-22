91c69a7e3f64b9ab5dc41d0f1d1817233ffdf571
tommy.huang <cy94295@gmail.com>
2022-05-22 21:24:45 +0800

    feat: array advanced

--- hello_ruby/basics/array.rb
+++ hello_ruby/basics/array.rb
@@ -4,20 +4,20 @@ puts friends[-1]
 puts friends.last
 puts friends.first
 
-list = [1,2,3,4,5]
-p list.map {|i| i * 2 - 1}
+list = [1, 2, 3, 4, 5]
+p list.map { |i| i * 2 - 1 }
 print [*1..42]
 p [*1..42].sample(3)
 
 # 舊式的 hash 寫法
-old_hash = {:title => "Ruby", :price => 350}
+old_hash = { :title => "Ruby", :price => 350 }
 p old_hash
 # 新式的 hash 寫法
-new_hash = {title: "Ruby", price: 350}
+new_hash = { title: "Ruby", price: 350 }
 p new_hash
 p old_hash == new_hash # true
 
-profile = {name: "5xruby", age: 18, tel: "0983153268"}
+profile = { name: "5xruby", age: 18, tel: "0983153268" }
 # puts profile[name] # undefined local variable name
 puts profile[:name]
 puts profile[:age]
@@ -25,5 +25,31 @@ puts profile[:age]
 p profile
 print profile
 puts profile
+
+x = 1..100
+p x
+p x.to_a
+
+# splat operator *
+# https://medium.com/@eggyy1224/ruby%E7%9A%84splat-operator-f2fa4f7956c7
+# https://stackoverflow.com/questions/11006897/what-is-the-mean-of-and-flatten-in-ruby
+
+p [*1..22]
+p (1..22).methods
+
+# splat operator only for Hash **
+
+def go(x, *array, **hash)
+  p "go function"
+  p x
+  p array
+  p hash
+end
+
+go(1, 2, 3, { "name": "tommy" })
+
+# flatten
+# a function that can collapses the pairs into a on-dimensional sequence of values
+p [[1, 2], [3, 4]].flatten
 
 
