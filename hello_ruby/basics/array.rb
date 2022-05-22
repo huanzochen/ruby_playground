5c35faeea4424fff1423a172b1eea72d884cb6d6
tommy.huang <cy94295@gmail.com>
2022-05-22 23:08:46 +0800

    feat: array advanced

--- hello_ruby/basics/array.rb
+++ hello_ruby/basics/array.rb
@@ -29,6 +29,11 @@ puts profile
 x = 1..100
 p x
 p x.to_a
+# ! 是立即 mutate 值，但在這裡沒有生效，因為隔一個 to_a
+p x.to_a.shuffle!
+z = x.to_a.shuffle!
+p "z"
+p z
 
 # splat operator *
 # https://medium.com/@eggyy1224/ruby%E7%9A%84splat-operator-f2fa4f7956c7
@@ -38,7 +43,6 @@ p [*1..22]
 p (1..22).methods
 
 # splat operator only for Hash **
-
 def go(x, *array, **hash)
   p "go function"
   p x
@@ -51,5 +55,33 @@ go(1, 2, 3, { "name": "tommy" })
 # flatten
 # a function that can collapses the pairs into a on-dimensional sequence of values
 p [[1, 2], [3, 4]].flatten
+
+alphabet = "a".."z"
+alphabet_array = alphabet.to_a
+p alphabet_array
+p alphabet_array.join
+p alphabet_array.join("-")
+
+b = alphabet_array.join(",")
+p b
+p b.split
+p b.split(",")
+
+z = %w(my name is tommy and this is great)
+p z
+for i in z
+  print i + " "
+end
+z.each do |word|
+  print word + " "
+end
+p " "
+z.each { |food| print food.capitalize + " " }
+p z = (1..10).to_a.shuffle
+
+# 可以透過 .select 來批量選出符合標準的物件, 不需要寫 for 迴圈
+p z.select { |number| number.odd? }
+
+
 
 
